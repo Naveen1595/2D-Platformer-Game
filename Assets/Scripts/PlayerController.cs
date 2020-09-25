386c74e31c2dddb08d92abafc449bc2de947a99d
Naveen1595 <naveen.rtw1595@gmail.com>
2020-09-25 11:21:13 +0530

    Crouch Function added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,76 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public BoxCollider2D playerCollider;
+    float sizeOfY, sizeOfY_Crouch, offsetOfY, offsetOfY_Crouch;
+    int currentTime = 0;
+    int CrouchTime = 10;
+    public Animator animator;
     /* private void OnCollisionEnter2D(Collision2D collision)
      {
          Debug.Log("Collision" + collision.gameObject.name);
      }*/
-    public Animator animator;
-    private void Update()
+    public void Start()
+    {
+        playerCollider = playerCollider.GetComponent<BoxCollider2D>();
+        sizeOfY = playerCollider.size.y;
+        sizeOfY_Crouch = playerCollider.size.y - 0.6f;
+        offsetOfY = playerCollider.offset.y;
+        offsetOfY_Crouch = playerCollider.offset.y - 0.25f;
+    }
+
+    
+    public void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
-        Debug.Log("Horizontal : " + speed);
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        bool jump = false;
+        bool crouch = false;
+        bool crouch_down = false;
+        if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                jump = true;
+            }
+        else 
+        {
+                jump = false;
+        }
         
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            playerCollider.size = new Vector2(playerCollider.size.x, sizeOfY_Crouch);
+            playerCollider.offset = new Vector2(playerCollider.offset.x, offsetOfY_Crouch);
+            currentTime++;
+            Debug.Log("Current Time:" + currentTime);
+            if(currentTime >= CrouchTime)
+            {
+                crouch = true;
+                crouch_down = true;
+            }
+            else
+            {
+                crouch = true;
+                crouch_down = false;
+            } 
+        }
+        else
+        {
+            playerCollider.size = new Vector2(playerCollider.size.x, sizeOfY);
+            playerCollider.offset = new Vector2(playerCollider.offset.x, offsetOfY);
+            currentTime = 0;
+            crouch = false;
+            crouch_down = false;
+        }
+
+        
+        /*Debug.Log("Vertical : " + jump);
+        Debug.Log("Crouch : " + crouch);
+        Debug.Log("Horizontal : " + speed);*/
+
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        animator.SetBool("isJump", jump);
+        animator.SetBool("isCrouch", crouch);
+        animator.SetBool("isCrouch_down", crouch_down);
+
         Vector3 scale = transform.localScale;
         if (speed < 0)
         {
