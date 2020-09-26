4562a75c755bbd221821176f4add84cc5a21171f
Naveen1595 <naveen.rtw1595@gmail.com>
2020-09-26 11:13:51 +0530

    horizontal and vertical movement added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,46 +5,64 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public BoxCollider2D playerCollider;
-    float sizeOfY, sizeOfY_Crouch, offsetOfY, offsetOfY_Crouch;
+    float sizeOfY, sizeOfY_Crouch, offsetOfY, offsetOfY_Crouch, horizontal, vertical;
+    float speed = 5;
+    float jumpMovement = 100;
     int currentTime = 0;
     int CrouchTime = 10;
+    bool jump, crouch, crouch_down, crouchActionCheck = false;
     public Animator animator;
-    /* private void OnCollisionEnter2D(Collision2D collision)
-     {
-         Debug.Log("Collision" + collision.gameObject.name);
-     }*/
+    public Rigidbody2D rb2d;
+   
+    //Start
     public void Start()
     {
         playerCollider = playerCollider.GetComponent<BoxCollider2D>();
         sizeOfY = playerCollider.size.y;
-        sizeOfY_Crouch = playerCollider.size.y - 0.6f;
+        sizeOfY_Crouch = playerCollider.size.y - 0.8f;
         offsetOfY = playerCollider.offset.y;
-        offsetOfY_Crouch = playerCollider.offset.y - 0.25f;
+        offsetOfY_Crouch = playerCollider.offset.y - 0.4f;
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    
+    //Update
     public void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        bool jump = false;
-        bool crouch = false;
-        bool crouch_down = false;
-        if (Input.GetAxisRaw("Vertical") > 0)
-            {
-                jump = true;
-            }
-        else 
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        crouchActionCheck = Input.GetKey(KeyCode.LeftControl);
+
+        verticalMovementAnimation(vertical);
+        crouchAction(crouchActionCheck);
+        animatorPlayerFun();
+        transformPlayerFun();
+
+    }
+
+    //Vertical Movement Animation
+    private void verticalMovementAnimation(float vertical)
+    {
+        if (vertical > 0)
         {
-                jump = false;
+            jump = true;
+            rb2d.AddForce(new Vector2(0f, jumpMovement), ForceMode2D.Force);
         }
-        
-        if (Input.GetKey(KeyCode.LeftControl))
+        else
+        {
+            jump = false;
+        }
+    }
+
+    //Crouch Action Animation
+    void crouchAction(bool crouchActionCheck)
+    {
+        if (crouchActionCheck)
         {
             playerCollider.size = new Vector2(playerCollider.size.x, sizeOfY_Crouch);
             playerCollider.offset = new Vector2(playerCollider.offset.x, offsetOfY_Crouch);
             currentTime++;
             Debug.Log("Current Time:" + currentTime);
-            if(currentTime >= CrouchTime)
+            if (currentTime >= CrouchTime)
             {
                 crouch = true;
                 crouch_down = true;
@@ -53,7 +71,7 @@ public class PlayerController : MonoBehaviour
             {
                 crouch = true;
                 crouch_down = false;
-            } 
+            }
         }
         else
         {
@@ -63,33 +81,47 @@ public class PlayerController : MonoBehaviour
             crouch = false;
             crouch_down = false;
         }
+    }
 
-        
-        /*Debug.Log("Vertical : " + jump);
-        Debug.Log("Crouch : " + crouch);
-        Debug.Log("Horizontal : " + speed);*/
-
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-        animator.SetBool("isJump", jump);
-        animator.SetBool("isCrouch", crouch);
-        animator.SetBool("isCrouch_down", crouch_down);
-
+    //Transform Function
+    void transformPlayerFun()
+    {
+        //scale transform
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
             animator.SetBool("isRunning", true);
         }
-        else if(speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
             animator.SetBool("isRunning", true);
         }
-        else if(speed == 0)
+        else if (horizontal == 0)
         {
             animator.SetBool("isRunning", false);
         }
 
         transform.localScale = scale;
+
+        //Horizontal Position transform 
+        Vector3 position = transform.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+
+        transform.position = position;
+
+        //Vertical Position
+        
+            
+    }
+
+    //Animator Function
+    void animatorPlayerFun()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        animator.SetBool("isJump", jump);
+        animator.SetBool("isCrouch", crouch);
+        animator.SetBool("isCrouch_down", crouch_down);
     }
 }
