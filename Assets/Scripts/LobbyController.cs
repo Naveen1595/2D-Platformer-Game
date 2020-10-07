224318a10f11bef7617dfc852a4bfe0869dcd4be
Naveen1595 <naveen.rtw1595@gmail.com>
2020-10-07 13:23:35 +0530

    Lobby Scene added

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    public Button PlayButton;
+    private void Awake()
+    {
+        PlayButton.onClick.AddListener(LevelOne);
+    }
+    
+    public void LevelOne()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
